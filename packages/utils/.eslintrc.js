f38e7428b5dc0b996ad73a9be1a6caead3bda833
qiaoyuwen <qiao.yuwen@foundbyte.com>
2021-12-06 16:07:35 +0800

    feat: eslint

--- packages/utils/.eslintrc.js
+++ packages/utils/.eslintrc.js
@@ -2,6 +2,6 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 
 // Add your "extends" boilerplate here, for example:
 module.exports = {
-  extends: [require.resolve('@monorepo-demo/eslint/src')],
+  extends: [require.resolve('@monorepo-demo/eslint/dist')],
   parserOptions: { tsconfigRootDir: __dirname }
 };
